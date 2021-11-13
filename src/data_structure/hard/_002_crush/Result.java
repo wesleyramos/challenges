763188ab65da5b6323c1c3b63568fc9326e2da04
Wesley <wesley.goncalves@furukawaelectric.com>
2021-11-13 18:14:40 -0300

    _002_crush - version 3 - improve approach

--- src/data_structure/hard/_002_crush/Result.java
+++ src/data_structure/hard/_002_crush/Result.java
@@ -5,23 +5,27 @@ import java.util.List;
 class Result {
 
     public static long arrayManipulation(int n, List<List<Integer>> queries) {
-        int[] array = new int[n];
-        int bigger = 0;
-        for (int j = 0; j < array.length; j++) {
-            for (int i = 0; i < queries.size(); i++) {
-                int start = queries.get(i).get(0) - 1;
-                int end = queries.get(i).get(1) - 1;
-                int value = queries.get(i).get(2);
+        int[] array = new int[n+1];
 
-                if (j >= start && j <= end) {
-                    array[j] += value;
-                }
-            }
-            if (array[j] > bigger) {
-                bigger = array[j];
+
+        for (int i = 0; i < queries.size(); i++) {
+            int start = queries.get(i).get(0) - 1;
+            int end = queries.get(i).get(1);
+            int value = queries.get(i).get(2);
+
+            array[start] += value;
+            array[end] -= value;
+
+        }
+        int bigger = 0;
+
+        for(int i = 0; i < array.length - 1; i++) {
+            int sum = array[i] + array[i+1];
+            array[i+1] += array[i];
+            if(sum > bigger) {
+                bigger = sum;
             }
         }
-
 
         return bigger;
     }
