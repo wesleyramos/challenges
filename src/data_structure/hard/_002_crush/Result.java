11c16aab9561d33929fe825550444ed8509ee006
Wesley <wesley.goncalves@furukawaelectric.com>
2021-11-13 18:00:19 -0300

    _002_crush - second version - brutal force

--- src/data_structure/hard/_002_crush/Result.java
+++ src/data_structure/hard/_002_crush/Result.java
@@ -5,25 +5,23 @@ import java.util.List;
 class Result {
 
     public static long arrayManipulation(int n, List<List<Integer>> queries) {
-        // Write your code here
         int[] array = new int[n];
+        int bigger = 0;
+        for (int j = 0; j < array.length; j++) {
+            for (int i = 0; i < queries.size(); i++) {
+                int start = queries.get(i).get(0) - 1;
+                int end = queries.get(i).get(1) - 1;
+                int value = queries.get(i).get(2);
 
-        for (int i = 0; i < queries.size(); i++) {
-            int start = queries.get(i).get(0);
-            int end = queries.get(i).get(1);
-            int value = queries.get(i).get(2);
-
-            for (int j = start - 1; j < end; j++) {
-                array[j] = array[j] + value;
+                if (j >= start && j <= end) {
+                    array[j] += value;
+                }
+            }
+            if (array[j] > bigger) {
+                bigger = array[j];
             }
         }
 
-        int bigger = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (bigger < array[i]) {
-                bigger = array[i];
-            }
-        }
 
         return bigger;
     }
